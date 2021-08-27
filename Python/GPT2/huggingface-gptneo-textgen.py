52a15b54254698daf4cf5f86ed1dc08113e0ef38
Bart Czernicki <bartczernicki@outlook.com>
2021-08-27 16:41:44 -0400

    Added deviceID parameter

--- Python/GPT2/huggingface-gptneo-textgen.py
+++ Python/GPT2/huggingface-gptneo-textgen.py
@@ -5,14 +5,19 @@
 ########################
 
 from transformers import pipeline
+import torch
 
+# Set seed for reproducability, only for non-deterministic steps
+torch.manual_seed(100)
+# Determine if to use GPU or CPU for compute
+deviceId = 0 if torch.cuda.is_available() else -1
 
 ################################
 ## TEXT GENARATION            ##
 ################################
 sentenceStart = 'You should consider learning statistics before approaching data science.'
 
-generator = pipeline("text-generation", model="EleutherAI/gpt-neo-1.3B")
+generator = pipeline("text-generation", model="EleutherAI/gpt-neo-1.3B", device=deviceId)
 generatorResults = generator(
     sentenceStart,
     do_sample=True, 
