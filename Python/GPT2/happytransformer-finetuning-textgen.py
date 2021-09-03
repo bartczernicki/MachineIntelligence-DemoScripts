132eafd25bf4e70b710f7df9d9585cd0bb645b22
Bart Czernicki <bartczernicki@outlook.com>
2021-09-03 01:33:52 +0000

    Merge branch 'main' of https://github.com/bartczernicki/MachineIntelligence-DemoScripts

--- Python/GPT2/happytransformer-finetuning-textgen.py
+++ Python/GPT2/happytransformer-finetuning-textgen.py
@@ -10,12 +10,20 @@ import torch
 import os
 import time
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 # Ensure all the CPU threads are being used
-torch.set_num_threads(18)
+#torch.set_num_threads(18)
 pyTorchThreads = torch.get_num_threads()
 print("PyTorch number of CPU threads: " + str(pyTorchThreads))
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print("Model Device Type: " + device)
+
+t = torch.cuda.get_device_properties(0).total_memory
+r = torch.cuda.memory_reserved(0)
+a = torch.cuda.memory_allocated(0)
+f = r-a  # free inside reserved
+print(f)
 
 # happy_gen = HappyGeneration("GPT2", "gpt2-xl")  # Best performance 
 
@@ -37,10 +45,10 @@ torch.manual_seed(255)
 
 baseModelType = "GPT2"
 
-baseModelArchitecture = "EleutherAI/gpt-neo-125M" # Smaller model
+#baseModelArchitecture = "EleutherAI/gpt-neo-125M" # Smaller model
 #baseModelArchitecture = "EleutherAI/gpt-neo-1.3B" # Larger model
 #baseModelArchitecture = "EleutherAI/gpt-neo-2.7B" # Larger model
-#baseModelArchitecture = "gpt2-xl"
+baseModelArchitecture = "gpt2"
 
 fineTunedModelLocation = r"Models\HappyTransformer-FineTuning-TextGen"
 
@@ -63,7 +71,7 @@ startTime = time.time()
 happy_gen = HappyGeneration(baseModelType, baseModelArchitecture)
 
 # Set up configuration for the model
-args = GENTrainArgs(num_train_epochs=50, batch_size=102) 
+args = GENTrainArgs(num_train_epochs=1000, batch_size=1)
 
 # # Traid the model
 happy_gen.train(r"Data\statisticslines.txt", args=args)
@@ -73,7 +81,9 @@ happy_gen.save(fineTunedModelLocation)
 
 # Print elapsed time
 timeElapsed = round(time.time() - startTime, 2)
-print("Time elapsed fine-tuning model: ", timeElapsed)
+print("*********************************")
+print("Time elapsed fine-tuning model: " + str(timeElapsed))
+print("*********************************")
 
 # ##########################
 # ## TEST THE TUNED MODEL ##
