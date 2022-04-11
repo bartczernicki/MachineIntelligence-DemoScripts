ff1c17b1ee754a5c13e344f4971bf843f3b4ca99
Bart Czernicki <bartczernicki@outlook.com>
2022-04-11 08:35:37 -0400

    Updated to script

--- Python/Baseball/BaseballHof-LightGbm.py
+++ Python/Baseball/BaseballHof-LightGbm.py
@@ -6,19 +6,20 @@ import pandas as pd
 baseballPlayers = pd.read_csv('MLBBaseballBattersFullTraining.csv')
 
 # Drop un-needed features
-baseballPlayers.drop('InductedToHallOfFame', axis=1, inplace=True)
 baseballPlayers.drop('FullPlayerName', axis=1, inplace=True)
 baseballPlayers.drop('LastYearPlayed', axis=1, inplace=True)
 baseballPlayers.drop('ID', axis=1, inplace=True)
 
 # Convert bool to 1/0
 # print(baseballPlayers['OnHallOfFameBallot'].dtypes)
-y_train = baseballPlayers.OnHallOfFameBallot.eq(True).mul(1)
+y_train_OnHallOfFameBallot = baseballPlayers.OnHallOfFameBallot.eq(True).mul(1)
+y_train_InductedToHallOfFame = baseballPlayers.InductedToHallOfFame.eq(True).mul(1)
 baseballPlayers.drop('OnHallOfFameBallot', axis=1, inplace=True)
+baseballPlayers.drop('InductedToHallOfFame', axis=1, inplace=True)
 
 # Print Results
 print(baseballPlayers.head(n=10).to_string(index=False))
-print(y_train.head(n=10).to_string(index=False))
+print(y_train_InductedToHallOfFame.head(n=10).to_string(index=False))
 
 # # Initialize an AutoML instance
 automl = AutoML()
@@ -38,7 +39,7 @@ automl_settings = {
 
 
 # # # Train with labeled input data
-automl.fit(X_train=baseballPlayers, y_train=y_train, **automl_settings)
+automl.fit(X_train=baseballPlayers, y_train=y_train_InductedToHallOfFame, **automl_settings)
 
 
 # # # Predict
