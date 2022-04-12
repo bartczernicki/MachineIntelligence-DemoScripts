2773b30f7343bab0ebf514bcd86a19db1b98a63e
Bart Czernicki <bartczernicki@outlook.com>
2022-04-11 22:05:56 -0400

    Updates

--- Python/Baseball/BaseballHof-LightGbm.py
+++ Python/Baseball/BaseballHof-LightGbm.py
@@ -2,7 +2,14 @@ from flaml import AutoML
 from flaml.data import load_openml_dataset
 from sklearn.datasets import load_iris
 import pandas as pd
+#import ray
+import time
 
+# Initialize Multiple CPU Cores
+# https://docs.ray.io/en/master/ray-overview/installation.html#m1-mac-apple-silicon-support
+
+
+# Read in CSV
 baseballPlayers = pd.read_csv('MLBBaseballBattersFullTraining.csv')
 
 # Drop un-needed features
@@ -24,17 +31,21 @@ print(y_train_InductedToHallOfFame.head(n=10).to_string(index=False))
 # # Initialize an AutoML instance
 automl = AutoML()
 # Specify automl goal and constraint
+# Info: https://microsoft.github.io/FLAML/docs/Use-Cases/Task-Oriented-AutoML
 automl_settings = {
-    "time_budget": 100,  # total running time in seconds
-    "metric": 'accuracy',  # primary metrics for regression can be chosen from: ['mae','mse','r2']
-    "estimator_list": ['lgbm', 'catboost', 'rf', 'extra_tree'],  # list of ML learners; we tune lightgbm in this example
+    "time_budget": 500,  # total running time in seconds
+    "metric": 'f1',  # primary metrics for regression can be chosen from: ['mae','mse','r2']
+    "estimator_list": ['lgbm', 'xgboost', 'catboost', 'rf', 'extra_tree', 'lrl1', 'kneighbor'],  # list of ML learners
+    #"estimator_list": ['lrl1', 'lrl2', 'kneighbor'], # list of ML learners
     "task": 'classification',  # task type  
     "log_file_name": 'baseball_experiment.log',  # flaml log file
     "log_type": 'all',
-    "seed": 200,    # random seed
+    "seed": 200, # random seed
     "n_jobs": 1,
+    # "n_concurrent_trials": 4,
     "eval_method": 'cv',
-    "n_splits": 4
+    "n_splits": 10
+    # "ensemble": True
 }
 
 
